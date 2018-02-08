5c7ac70f08183bf748601453084eef8298409164
James Carr <jamescarr@uk.ibm.com>
2018-02-08 12:38:28 +0000

    Upgraded web app

--- public/resources/app.js
+++ public/resources/app.js
@@ -4,37 +4,43 @@ var baseUrl = '/components/views/';
 fetchData().then(bootstrapApp);
 
 function fetchData() {
-    var initInjector = angular.injector(["ng"]);
-    var $http = initInjector.get("$http");
+	var initInjector = angular.injector(["ng"]);
+	var $http = initInjector.get("$http");
 
-    return $http.get("catalog/config").then(function(response) {
-        app.constant("CONFIG", response.data);
-        console.log("Boostrap success")
-    }, function(error) {
-        console.log("Error bootstraping application: "+ error)
-    });
+	return $http.get("catalog/config").then(function(response) {
+		app.constant("CONFIG", response.data);
+		console.log("Boostrap success")
+	}, function(error) {
+		console.log("Error bootstraping application: " + error)
+	});
 }
 
 function bootstrapApp() {
-    angular.element(document).ready(function() {
-        angular.bootstrap(document, ["lightbluecompute"]);
-    });
+	angular.element(document).ready(function() {
+		angular.bootstrap(document, ["lightbluecompute"]);
+	});
 }
 
 app.run(function($rootScope) {
-  window.onbeforeunload = function(event) {
-    $rootScope.$broadcast('savestate');
-  };
+	window.onbeforeunload = function(event) {
+		$rootScope.$broadcast('savestate');
+	};
 });
 
 app.config(['$routeProvider', function($routeProvider) {
-    console.log("entering Angular config");
-
-    $routeProvider.when('/home', {
-       templateUrl : baseUrl + 'home.html',
-       controller: 'HomeController'
-    })
-    .otherwise({
-        redirectTo: '/home'
-    });
+	$routeProvider.when('/home', {
+		templateUrl: baseUrl + 'home.html',
+		controller: 'HomeController'
+	}).when('/catalog', {
+		templateUrl: baseUrl + 'catalog.html',
+		controller: 'CatalogController'
+	}).when('/item/:id', {
+		templateUrl: baseUrl + 'item.html',
+		controller: 'ItemController'
+	}).when('/customer', {
+		templateUrl: baseUrl + 'customer.html',
+		controller: 'CustomerController'
+	}).otherwise({
+		redirectTo: '/home'
+	});
 }]);
